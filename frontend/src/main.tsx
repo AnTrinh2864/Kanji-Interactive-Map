c568dda3a53692785247cb485f56bd7056fcb260
AnTrinh2864 <hongbaoan9502@gmail.com>
2025-10-23 15:53:41 +0700

    three

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { NewApp } from './components/KanjiBoard/Three/NewApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NewApp />
   </StrictMode>,
 )
